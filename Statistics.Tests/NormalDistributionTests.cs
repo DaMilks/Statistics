5e027353a080c4da3add7d8a379374919478571e
DaMilks <danya.boldyrev.89@mail.ru>
2022-05-15 15:48:21 +0300

    NormalDistribution test with valid data

---
+++ Statistics.Tests/NormalDistributionTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Statistics.Distributions;
+
+namespace Statistics.Tests
+{
+    public class NormalDistributionTests
+    {
+        [Test]
+        /// <summary>
+        /// Can create normal distribution
+        /// </summary>
+        /// <param name="expected">Mean value.</param>
+        /// <param name="sigma">Standard deviation value.</param>
+        [TestCase(1, 10)]
+        [TestCase(1, -5)]
+        [TestCase(1, 10)]
+        [TestCase(10.0, -5.0)]
+        [TestCase(double.PositiveInfinity, 0)]
+        public void CanCreateNormal(double sigma, double expected)
+        {
+            IDistribution n = new NormalDistribution(sigma, expected);
+            Assert.Multiple(() =>
+            {
+                Assert.That(n.Mean, Is.EqualTo(expected));
+                Assert.That(n.StdDev, Is.EqualTo(sigma));
+            });
+        }
+    }
+}
