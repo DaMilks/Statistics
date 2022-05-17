86885037a4f5918dabf664a9bb880e11d13e4c8d
DaMilks <danya.boldyrev.89@mail.ru>
2022-05-17 21:54:39 +0300

    Fix typo CauchyDistribution

---
+++ Statistics.Tests/DistributionsTests/CauchyDistributionTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Statistics.Distributions;
+
+namespace Statistics.Tests.DistributionsTests
+{
+    [TestFixture]
+    public class CauchyDistributionTests
+    {
+        /// <summary>
+        /// Can create standard cauchy distribution instance
+        /// </summary>
+        [Test]
+        public void CanCreateStandardCauchyDistribution()
+        {
+            CauchyDistribution n = new();
+            Assert.Multiple(() =>
+            {
+                Assert.That(n.Minimum, Is.EqualTo(0.0));
+                Assert.That(n.Maximum, Is.EqualTo(1.0));
+            });
+        }
+    }
+}
