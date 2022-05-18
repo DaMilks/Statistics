0f112de92c13ebe73270321da81be94e19e1d152
DaMilks <danya.boldyrev.89@mail.ru>
2022-05-18 23:12:50 +0300

    Categorial distribution create instance by number of options

--- Statistics.Tests/DistributionsTests/CategorialDistributionTests.cs
+++ Statistics.Tests/DistributionsTests/CategorialDistributionTests.cs
@@ -16,6 +16,22 @@ namespace Statistics.Tests.DistributionsTests
             double[] prob = { 0.5, 0.5 };
             Assert.That(n.Probabilities, Is.EqualTo(prob));
         }
-        
+        [Test]
+        /// <summary>
+        /// Can create categorial distribution instance by number of options
+        /// </summary>
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(10)]
+        public void CanCreateByNumberCategorialDistribution(int numberOfOptions)
+        {
+            CategorialDistribution n = new(numberOfOptions);
+            Assert.Multiple(() =>
+            {
+                Assert.That(n.Probabilities, !Is.Null);
+                Assert.That(n.Probabilities[0], Is.EqualTo(1.0/numberOfOptions));
+            });
+        }
+
     }
 }
