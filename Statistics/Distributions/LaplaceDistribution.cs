0843aedaffb8621e482f2a5eb7d935d5697359e3
DaMilks <danya.boldyrev.89@mail.ru>
2022-06-03 08:24:20 +0300

    Documentation

--- Statistics/Distributions/LaplaceDistribution.cs
+++ Statistics/Distributions/LaplaceDistribution.cs
@@ -2,6 +2,9 @@
 
 namespace Statistics.Distributions
 {
+    /// <summary>
+    /// Normal distribution(Gaussian distribution)
+    /// </summary>
     public class LaplaceDistribution : IContiniousDistribusion
     {
         private readonly double _location, _scale;
@@ -12,7 +15,9 @@ namespace Statistics.Distributions
         {
             return !double.IsNaN(location) & scale > 0;
         }
-
+        /// <summary>
+        /// Initializes a new instance of the Laplace distribution with given location and scale values
+        /// </summary>
         public LaplaceDistribution(double location, double scale)
         {
             if (!IsValidParameters(location, scale))
@@ -21,48 +26,86 @@ namespace Statistics.Distributions
             _scale = scale;
             _random = new();
         }
+        /// <summary>
+        /// Initializes a new instance of the Laplace distribution with given scale value and location=0
+        /// </summary>
         public LaplaceDistribution(double scale) : this(0, scale)
         {
 
         }
-
+        /// <summary>
+        /// Initializes a new instance of the Laplace distribution with given location, scale values and randomsource
+        /// </summary>
         public LaplaceDistribution(double location, double scale, Random random) : this(location, scale)
         {
             _random = random;
         }
-
+        /// <summary>
+        /// Initializes a new instance of the Laplace distribution with location=0 and scale=1
+        /// </summary>
         public LaplaceDistribution() : this(0, 1) { }
-
+        /// <summary>
+        /// Gets the mode of the distribution.
+        /// </summary>
         public double Mode => _location;
-
+        /// <summary>
+        /// Gets the smallest element which can be maked
+        /// </summary>
         public double Minimum => double.NegativeInfinity;
-
+        /// <summary>
+        /// Gets the largest element which can be maked
+        /// </summary>
         public double Maximum => double.PositiveInfinity;
-
+        /// <summary>
+        /// Gets the random number generator which is used to make random samples.
+        /// </summary>
         public Random Random => _random;
-
+        /// <summary>
+        /// Gets the mean of the distribution.
+        /// </summary>
         public double Mean => _location;
-
-        public double Variance => 2*_scale*_scale;
-
+        /// <summary>
+        /// Gets the variance of the distribution.
+        /// </summary>
+        public double Variance => 2 * _scale * _scale;
+        /// <summary>
+        /// Gets the standard deviation of the distribution.
+        /// </summary>
         public double StdDev => Math.Sqrt(Variance);
-
+        /// <summary>
+        /// Gets the median of the distribution.
+        /// </summary>
         public double Median => _location;
-
+        /// <summary>
+        /// Gets the location of the distribution.
+        /// </summary>
         public double Location => _location;
-
+        /// <summary>
+        /// Gets the scale of the distribution.
+        /// </summary>
         public double Scale => _scale;
-
+        /// <summary>
+        /// Computes the cumulative distribution (CDF) of the distribution at x
+        /// </summary>
+        /// /// <param name="x">The location at which to compute the cumulative distribution function.</param>
+        /// <returns>the cumulative distribution at location <paramref name="x"/>.</returns>
         public double CumulativeDistribution(double x)
         {
             return 0.5 * (1 + (Math.Sign(x - _location) * (1 - Math.Exp(-Math.Abs(x - _location) / _scale))));
         }
-
+        /// <summary>
+        /// Computes the probability density of the distribution (PDF)
+        /// </summary>
+        /// <param name="x">The location at which to compute the density.</param>
+        /// <returns>the density at <paramref name="x"/>.</returns>
         public double Density(double x)
         {
             return Math.Exp(-Math.Abs(x - _location) / _scale) / (2 * _scale);
         }
-
+        /// <summary>
+        /// Makes a random sample from Normal distribution.
+        /// </summary>
+        /// <returns>a sample from Normal distribution.</returns>
         public double MakeSample()
         {
             double basedValue = _random.NextDouble();
