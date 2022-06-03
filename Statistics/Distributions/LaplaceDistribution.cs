61a21b41392ee80c63ccca555ca2b8093981eeae
DaMilks <danya.boldyrev.89@mail.ru>
2022-06-03 07:45:18 +0300

    LaplaceDistribution ralized class

--- Statistics/Distributions/LaplaceDistribution.cs
+++ Statistics/Distributions/LaplaceDistribution.cs
@@ -7,21 +7,47 @@ namespace Statistics.Distributions
         private readonly double _location, _scale;
 
         private readonly Random _random;
-        public double Mode => throw new NotImplementedException();
 
-        public double Minimum => throw new NotImplementedException();
+        private static bool IsValidParameters(double location, double scale)
+        {
+            return !double.IsNaN(location) & scale > 0;
+        }
 
-        public double Maximum => throw new NotImplementedException();
+        public LaplaceDistribution(double location, double scale)
+        {
+            if (!IsValidParameters(location, scale))
+                throw new ArgumentException("Invalid parametrization for the distribution.");
+            _location = location;
+            _scale = scale;
+            _random = new();
+        }
+        public LaplaceDistribution(double scale) : this(0, scale)
+        {
 
-        public Random Random => throw new NotImplementedException();
+        }
 
-        public double Mean => throw new NotImplementedException();
+        public LaplaceDistribution(double location, double scale, Random random) : this(location, scale)
+        {
+            _random = random;
+        }
 
-        public double Variance => throw new NotImplementedException();
+        public LaplaceDistribution() : this(0, 1) { }
 
-        public double StdDev => throw new NotImplementedException();
+        public double Mode => _location;
 
-        public double Median => throw new NotImplementedException();
+        public double Minimum => double.NegativeInfinity;
+
+        public double Maximum => double.PositiveInfinity;
+
+        public Random Random => _random;
+
+        public double Mean => _location;
+
+        public double Variance => 2*_scale*_scale;
+
+        public double StdDev => Math.Sqrt(Variance);
+
+        public double Median => _location;
 
         public double Location => _location;
 
@@ -29,17 +55,18 @@ namespace Statistics.Distributions
 
         public double CumulativeDistribution(double x)
         {
-            throw new NotImplementedException();
+            return 0.5 * (1 + (Math.Sign(x - _location) * (1 - Math.Exp(-Math.Abs(x - _location) / _scale))));
         }
 
         public double Density(double x)
         {
-            throw new NotImplementedException();
+            return Math.Exp(-Math.Abs(x - _location) / _scale) / (2 * _scale);
         }
 
         public double MakeSample()
         {
-            throw new NotImplementedException();
+            double basedValue = _random.NextDouble();
+            return _location - _scale * Math.Sign(basedValue - 0.5) * Math.Log(1 - (2 * Math.Abs(basedValue - 0.5)));
         }
     }
 }
