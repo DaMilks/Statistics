ae062e29c35a52f25040ce16dfd2085a35794128
DaMilks <danya.boldyrev.89@mail.ru>
2022-06-03 07:21:39 +0300

    LaplaceDistribution base structure

---
+++ Statistics/Distributions/LaplaceDistribution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Statistics.Distributions
+{
+    public class LaplaceDistribution : IContiniousDistribusion
+    {
+        private readonly double _location, _scale;
+
+        private readonly Random _random;
+        public double Mode => throw new NotImplementedException();
+
+        public double Minimum => throw new NotImplementedException();
+
+        public double Maximum => throw new NotImplementedException();
+
+        public Random Random => throw new NotImplementedException();
+
+        public double Mean => throw new NotImplementedException();
+
+        public double Variance => throw new NotImplementedException();
+
+        public double StdDev => throw new NotImplementedException();
+
+        public double Median => throw new NotImplementedException();
+
+        public double Location => _location;
+
+        public double Scale => _scale;
+
+        public double CumulativeDistribution(double x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Density(double x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double MakeSample()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
