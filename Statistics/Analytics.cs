96f725e38e56a53ba043e7e47fb05ee1d67fc8c9
DaMilks <danya.boldyrev.89@mail.ru>
2022-06-12 10:57:53 +0300

    Analytics: realized Mode and Median

--- Statistics/Analytics.cs
+++ Statistics/Analytics.cs
@@ -138,9 +138,101 @@ namespace Statistics
         {
             return Math.Sqrt(Variance(values));
         }
+
         public static double StdDev(int[] values)
         {
             return Math.Sqrt(Variance(values));
         }
+
+        public static double Median(double[] values)
+        {
+            //TODO:Realize faster method https://ru.wikipedia.org/wiki/Алгоритм_выбора
+            double[] valuesSorted = new double[values.Length];
+            Array.Copy(values,valuesSorted, values.Length);
+            Array.Sort(valuesSorted);
+            return MedianSorted(valuesSorted);
+        }
+
+        public static double MedianSorted(double[] values)
+        {
+            int k = values.Length / 2;
+            if ((values.Length & 1) == 0x1)
+                return values[k];
+            else return (values[k - 1] + values[k]) / 2d;
+        }
+
+        public static double Median(int[] values)
+        {
+            int[] valuesSorted = new int[values.Length];
+            Array.Copy(values, valuesSorted, values.Length);
+            Array.Sort(valuesSorted);
+            return MedianSorted(valuesSorted);
+        }
+
+        public static double MedianSorted(int[] values)
+        {
+            int k = values.Length / 2;
+            if ((values.Length & 1) == 0x1)
+                return values[k];
+            else return (values[k - 1] + values[k]) / 2d;
+        }
+
+        public static int Mode(int[] values)
+        {
+            int min = Minimum(values),
+            max = Maximum(values),
+            n = max - min + 1;
+            int[] histogram = new int[n],
+            possibleValues = new int[n];
+            for (int i = 0; i < max - min; i++)
+            {
+                possibleValues[i] = min + i;
+                histogram[i] = 0;
+            }
+            foreach (int v in values)
+                for (int i = 0; i < max - min; i++)
+                {
+                    if (v == possibleValues[i])
+                        histogram[i]++;
+                }
+            max = histogram[0];
+            int k = 0;
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                if (histogram[i] > max)
+                {
+                    max = histogram[i];
+                    k = i;
+                }
+            }
+            return possibleValues[k];
+        }
+
+        public static int Mode(int[] values, int[] possibleValues)
+        {
+            int[] histogram = new int[possibleValues.Length];
+            for (int i = 0; i < possibleValues.Length; i++)
+            {
+                histogram[i] = 0;
+            }
+            foreach (int v in values)
+                for (int i = 0; i < possibleValues.Length; i++)
+                {
+                    if (v == possibleValues[i])
+                        histogram[i]++;
+                }
+            int max = histogram[0];
+            int k = 0;
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                if (histogram[i] > max)
+                {
+                    max = histogram[i];
+                    k = i;
+                }
+            }
+            return possibleValues[k];
+        }
+
     }
 }
